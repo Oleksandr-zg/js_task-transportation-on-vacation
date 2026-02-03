77cee624e0751d0c632d9cece8f504eb1270b8e3
Aleksandr <zgyrski202@gmail.com>
2026-02-03 13:27:41 +0200

    added consts, replacement else if

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,14 +3,29 @@
  *
  * @return {number}
  */
+const COST_PER_DAY = 40;
+const DISCOUNT_1 = 20;
+const DISCOUNT_2 = 50;
+const NUM_1 = 1;
+const NUM_3 = 3;
+const NUM_7 = 7;
+
 function calculateRentalCost(days) {
-  if (days >= 3 && days < 7) {
-    return days * 40 - 20;
-  } else if (days >= 7) {
-    return days * 40 - 50;
-  } else {
-    return days * 40;
+  let result = 0;
+
+  if (days >= NUM_3 && days < NUM_7) {
+    result = days * COST_PER_DAY - DISCOUNT_1;
   }
+
+  if (days >= NUM_7) {
+    result = days * COST_PER_DAY - DISCOUNT_2;
+  }
+
+  if (days >= NUM_1 && days < NUM_3) {
+    result = days * COST_PER_DAY;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
